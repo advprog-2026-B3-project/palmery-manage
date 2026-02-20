ea5360cb6b1739d2ec617dd3a993605d59c7a75b
Maira <mairaazsh@gmail.com>
2026-02-20 10:25:27 +0700

    Init SpringBoot

---
+++ src/main/java/id/ac/ui/cs/advprog/palmerymanage/PalmeryManageApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.palmerymanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalmeryManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalmeryManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/palmerymanage/PalmeryManageApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.palmerymanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalmeryManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
