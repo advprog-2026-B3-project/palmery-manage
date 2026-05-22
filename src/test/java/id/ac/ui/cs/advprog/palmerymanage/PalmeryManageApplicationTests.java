86753d18c2c83fe502e432bda2c8c5a190cd9703
gung16 <gung.160906@gmail.com>
2026-05-22 23:24:55 +0700

    Add

--- src/test/java/id/ac/ui/cs/advprog/palmerymanage/PalmeryManageApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/palmerymanage/PalmeryManageApplicationTests.java
@@ -2,9 +2,14 @@ package id.ac.ui.cs.advprog.palmerymanage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class PalmeryManageApplicationTests {
+
+    @MockitoBean
+    private StringRedisTemplate stringRedisTemplate;
 
     @Test
     void contextLoads() {
